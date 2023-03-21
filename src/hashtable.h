85e2b07e253761d5e5b992b9d79c9d750cad95b9
Дмитрий Баннов <bannovd@yandex.ru>
2023-03-21 16:59:13 +0300

    add Insert method

--- src/hashtable.h
+++ src/hashtable.h
@@ -9,24 +9,26 @@
 template <typename K, typename V, std::size_t tableSize, typename HashFunc = KeyHash<K, tableSize>>
 class HashTable {
 public:
+    HashTable(const HashTable& other) = delete;
+    const HashTable& operator=(const HashTable& other) = delete;
     HashTable()
-        : table(), hashFunc() {}
+        : table_(), hashFunc_() {}
 
     ~HashTable() {
         for (size_t i = 0; i < tableSize; ++i) {
-            HashNode<K, V> *entry = table[i];
+            HashNode<K, V> *entry = table_[i];
             while (entry != nullptr) {
                 HashNode<K, V> *prev = entry;
                 entry = entry->getNext();
                 delete prev;
             }
-            table[i] = NULL;
+            table_[i] = NULL;
         }
     }
 
     std::optional<HashNode<K, V>> Get(const K& key) {
-        unsigned long hashValue = hashFunc(key);
-        HashNode<K, V> *entry = table[hashValue];
+        unsigned long hashValue = hashFunc_(key);
+        HashNode<K, V> *entry = table_[hashValue];
         while (entry != nullptr) {
             if (entry->Key() == key) {
                 return *entry;
@@ -36,12 +38,30 @@ public:
         return std::nullopt;
     }
 
+    void Insert(const K &key, const V &value) {
+        unsigned long hashValue = hashFunc_(key);
+        HashNode<K, V> *prev = NULL;
+        HashNode<K, V> *entry = table_[hashValue];
 
-private:
-    HashTable(const HashTable& other);
-    const HashTable& operator=(const HashTable& other);
-    HashNode<K, V> *table[tableSize];
-    HashFunc hashFunc;
+        while (entry != nullptr && entry->Key() != key) {
+            prev = entry;
+            entry = entry->Next();
+        }
+
+        if (entry == NULL) {
+            entry = new HashNode<K, V>(key, value);
+            if (prev == NULL) {
+                table_[hashValue] = entry;
+            } else {
+                prev->SetNext(entry);
+            }
+        } else {
+            entry->SetValue(value);
+        }
+    }
+private:    
+    HashNode<K, V>* table_[tableSize];
+    HashFunc hashFunc_;
 };
 
 #endif // HASHTABLE_H
